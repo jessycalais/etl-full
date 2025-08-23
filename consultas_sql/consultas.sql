a57cfc3697b6edcbfcf9e0e55593830ac5d0d6ef
Jéssica Barros <jessyca.lais.cdb@gmail.com>
2025-08-22 23:31:13 -0300

    Ajuste nas consultas SQL - Dia 2

--- consultas_sql/consultas.sql
+++ consultas_sql/consultas.sql
@@ -1,60 +1,62 @@
 /* Perguntas de Negócio */
 
 /* ============================================================== */
-
-/* QUESTÃO 01. Quais são os 5 clientes com maior valor total de ruptura (Valor Ruptura_$) no período analisado? */
+/* QUESTÃO 01. Quais são os 5 clientes com maior valor total de ruptura (Valor Ruptura_$) no período analisado?*/
 SELECT  
-    COD_CLIENTE AS CODIGO_CLIENTE,
-    SUM(VALOR_RUPTURA_MONETARIO) AS TOTAL_RUPTURA
-FROM `casegb-469522.varejo.ruptura_faltaproduto`
+    CLIENTE_DESCRICAO AS NOME_CLIENTE,
+    ROUND(SUM(VALOR_RUPTURA_MONETARIO), 4) AS TOTAL_RUPTURA
+FROM 
+    `casegb-469522.varejo.ruptura_faltaproduto`
 GROUP BY
-    COD_CLIENTE 
+    CLIENTE_DESCRICAO
 ORDER BY 
     SUM(VALOR_RUPTURA_MONETARIO) DESC
 LIMIT
     5;
 
 /* ============================================================== */
-
-/*  QUESTÃO 02.	No mês mais recente, qual é a categoria de produto com a maior cobertura média de estoque? */
+/* QUESTÃO 02. No mês mais recente, qual é a categoria de produto com a maior cobertura média de estoque?*/
 SELECT
+    MAX(MES) AS MES_MAIS_RECENTE,
     DESCRICAO_CATEGORIA,
-    AVG(COBERTURA_DIAS) AS MEDIA_COBERTURA
-FROM `casegb-469522.varejo.estoque`
--- Filtrar assim para garantir que o maior mês será usado, sem a necessidade de dizer que é '07'
+    ROUND(AVG(COBERTURA_DIAS), 4) AS MEDIA_COBERTURA
+FROM 
+    `casegb-469522.varejo.estoque`
+-- Filtrar assim para garantir que o maior mês será usado, sem a necessidade de dizer que é o mês de julho
 WHERE
-    CAST(MES AS INT64) IN (
-         SELECT
-            MAX(CAST(MES AS INT64)) AS MAIOR_MES
+-- Usar CAST(MES AS INT64) apenas se mês for do tipo texto. Ajustei tipo na "Transformação" no ETL
+    MES IN (
+        SELECT
+            MAX(MES) AS MES_MAIS_RECENTE
         FROM
             `casegb-469522.varejo.estoque`
     )
 GROUP BY
     DESCRICAO_CATEGORIA
 ORDER BY
-    2 DESC
+    3 DESC
 LIMIT 1;
 
 /* ============================================================== */
-
-/* QUESTÃO 03.	Qual foi o mês com o maior volume total de vendas (VLR_VOLUME_REAL)? */
+/* QUESTÃO 03.	Qual foi o mês com o maior volume total de vendas (VLR_VOLUME_REAL)?*/
 SELECT
     CASE MES
-        WHEN '01' THEN 'Janeiro'
-        WHEN '02' THEN 'Fevereiro'
-        WHEN '03' THEN 'Março'
-        WHEN '04' THEN 'Abril'
-        WHEN '05' THEN 'Maio'
-        WHEN '06' THEN 'Junho'
-        WHEN '07' THEN 'Julho'
-        WHEN '08' THEN 'Agosto'
-        WHEN '09' THEN 'Setembro'
-        WHEN '10' THEN 'Outubro'
-        WHEN '11' THEN 'Novembro'
-        WHEN '12' THEN 'Dezembro'
+        WHEN 1 THEN 'Janeiro'
+        WHEN 2 THEN 'Fevereiro'
+        WHEN 3 THEN 'Março'
+        WHEN 4 THEN 'Abril'
+        WHEN 5 THEN 'Maio'
+        WHEN 6 THEN 'Junho'
+        WHEN 7 THEN 'Julho'
+        WHEN 8 THEN 'Agosto'
+        WHEN 9 THEN 'Setembro'
+        WHEN 10 THEN 'Outubro'
+        WHEN 11 THEN 'Novembro'
+        WHEN 12 THEN 'Dezembro'
     END MES,
     SUM(VLR_VOLUME_REAL) AS VOLUME_TOTAL
-FROM `casegb-469522.varejo.vendas`
+FROM 
+    `casegb-469522.varejo.vendas`
 GROUP BY
     MES
 ORDER BY
@@ -62,7 +64,6 @@ ORDER BY
 LIMIT 1;
 
 /* ============================================================== */
-
 /* QUESTÃO 04.	Levante uma lista de clientes que, no último mês disponível na base, 
 tiveram uma taxa de ruptura (Ruptura_%) e uma cobertura de estoque em um nível que 
 pode representar um risco para o negócio. (fique livre para determinar os níveis de risco) */
@@ -73,18 +74,5 @@ pode representar um risco para o negócio. (fique livre para determinar os níve
 -- https://repositorio.fgv.br/server/api/core/bitstreams/be9ca766-e095-46af-98e1-3a8ea96b3d31/content;
 -- ii. Cobertura Média em Risco: menor do que 15 dias. Fiz essa escolha supondo que os produtos são abastecidos 
 -- a cada duas semanas.
-/* --------- */
-SELECT  
-    COD_CLIENTE,
-    CLIENTE_DESCRICAO,
-    MATERIAL_DESCRICAO_CATEGORIA AS CATEGORIA,
-    RUPTURA_PERCENTUAL,
-    COBERTURA_DIAS
-FROM
-    `casegb-469522.varejo.consolidado`
-WHERE
-    RUPTURA_PERCENTUAL > 0.1
-    AND COBERTURA_DIAS < 15
-ORDER BY
-    COD_CLIENTE,
-    CATEGORIA;
+
+-- REVISAR
